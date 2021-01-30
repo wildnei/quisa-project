a9bc7beaf4e59f8c1ecda6ea798872e3d7abea56
wildnei <wildneisuane@gmail.com>
2021-01-30 11:08:22 -0500

    first commit

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+    $.ajax({
+        url: "https://api.rawg.io/api/games?search=fallout&key=cbd50d896d3c44be9e52237725e53583",
+            method: "GET"
+        }).then(function (response) {
+        console.log(response);
+    });
+
+
+
+$.ajax({
+    url: "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15",
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
